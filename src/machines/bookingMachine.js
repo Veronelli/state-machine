df657263c46edc99bae86e1617ca1281e1b49bae
Veronelli <facu2000veronelli@outlook.com>
2024-01-05 18:22:38 -0300

    add actions

--- src/machines/bookingMachine.js
+++ src/machines/bookingMachine.js
@@ -1,32 +1,51 @@
-import { createMachine } from 'xstate';
+import { createMachine } from "xstate";
 
-const bookingMachine = createMachine({
-  id: 'buy plane tickets',
-  initial: 'initial',
-  states: {
-    initial: {
-      on: {
-        REGISTER: 'registerPatient'
-      }
+const bookingMachine = createMachine(
+  {
+    id: "buy plane tickets",
+    initial: "initial",
+    states: {
+      initial: {
+        on: {
+          REGISTER: { target: "registerPatient", actions: "printWelcome" },
+        },
+      },
+      registerPatient: {
+        exit: "printExit",
+        entry: "printEntry",
+        on: {
+          CONTIUNE: {
+            target: "selectAppointment",
+          },
+          RESET: "initial",
+        },
+      },
+      selectAppointment: {
+        on: {
+          DONE: "finishReservation",
+          RESET: "initial",
+        },
+      },
+      finishReservation: {
+        on: {
+          RESET: "initial",
+        },
+      },
     },
-    registerPatient: {
-      on: {
-        CONTIUNE: 'selectAppointment',
-        RESET: 'initial'
-      }
-    },
-    selectAppointment: {
-      on: {
-        DONE: 'finishReservation',
-        RESET: 'initial'
-      }
-    },
-    finishReservation: {
-      on: {
-        RESET: 'initial'
-      }
+  },
+  {
+    actions: {
+      printWelcome: () => {
+        console.log("Print Home");
+      },
+      printEntry: () => {
+        console.log("Print Entry");
+      },
+      printExit: () => {
+        console.log("Print Exit");
+      },
     },
   }
-})
+);
 
-export default bookingMachine
+export default bookingMachine;
